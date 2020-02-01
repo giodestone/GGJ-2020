b26c2b0b08065db056eb35f4ec22a8681e1280b9
JACK AITKEN <1800501@uad.ac.uk>
2020-02-01 00:46:34 +0000

    Added move scripts and menu script
    Added new menu script for navigation as well as for moving while in balloon and while on ground

--- Assets/Scripts/BalloonMove.cs
+++ Assets/Scripts/BalloonMove.cs
@@ -7,10 +7,15 @@ public class BalloonMove : MonoBehaviour
     [SerializeField]
     private float MoveSpeed = 5.0f;
 
+    private bool InBalloon = true;
+
+    [SerializeField]
+    private GroundMovement GroundPlayer; 
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GroundPlayer = GameObject.Find("GroundedPlayer").GetComponent<GroundMovement>();
     }
 
     // Update is called once per frame
@@ -21,49 +26,46 @@ public class BalloonMove : MonoBehaviour
 
     void CalculateMovement()
     {
-        float HorizontalInput = Input.GetAxis("Horizontal");
-        float VerticalInput = Input.GetAxis("Vertical");
+        if (InBalloon == true)
+        {
+            float HorizontalInput = Input.GetAxis("Horizontal");
+            float VerticalInput = Input.GetAxis("Vertical");
 
-        Vector3 Direction = new Vector3(HorizontalInput, VerticalInput);
+            Vector3 Direction = new Vector3(HorizontalInput, VerticalInput);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(0.0f, 0.0f, MoveSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0.0f, 0.0f, -MoveSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
-        }
-        else if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(0.0f, MoveSpeed * Time.deltaTime, 0.0f);
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(0.0f, 0.0f, MoveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(0.0f, 0.0f, -MoveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
+            }
+            else if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(0.0f, MoveSpeed * Time.deltaTime, 0.0f);
+            }
         }
     }
 
-    void TriggerDialogue(Collider other)
+    void SwitchMove(Collider other)
     {
-        if (other.tag == "Owner1" && Input.GetKey(KeyCode.A))
+        if (other.tag == "cloud" && InBalloon == true)
         {
-            //NPC 1 dialogue. Will add soon
+            InBalloon = false;
+            GroundPlayer.CalculateMovement();
         }
-        
-        if (other.tag == "Owner2" && Input.GetKey(KeyCode.A))
+        else if (InBalloon == false)
         {
-            //NPC 2 dialogue. Will add soon
+            InBalloon = true;
         }
-
-        if (other.tag == "Owner3" && Input.GetKey(KeyCode.A))
-        { 
-            //NPC 3 dialogue. Will add soon
-        }
-            
     }
 }
