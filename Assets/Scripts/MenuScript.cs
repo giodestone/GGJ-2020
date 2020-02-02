6d5145685442031603f235d039963ca89b6df75c
JACK EDGAR <1800575@uad.ac.uk>
2020-02-02 13:16:56 +0000

    Added Menu and Instructions

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
